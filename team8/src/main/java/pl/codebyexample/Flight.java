668730abcafa3d006a09f0d816e0af70f2e7740e
Andrzej Wislowski <andrzej.wislowski@allegro.pl>
2013-06-24 11:53:43 +0200

    in the middle of iteration 1 ..

--- team8/src/main/java/pl/codebyexample/Flight.java
+++ team8/src/main/java/pl/codebyexample/Flight.java
@@ -3,62 +3,67 @@ package pl.codebyexample;
 import java.util.HashSet;
 
 /**
- * Created with IntelliJ IDEA.
- * User: andrzej.wislowski
- * Date: 24.06.2013
- * Time: 10:56
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: andrzej.wislowski Date: 24.06.2013 Time:
+ * 10:56 To change this template use File | Settings | File Templates.
  */
 public class Flight {
 
-    String flightNumber;
-    HashSet<Seat> seats = new HashSet<Seat>();
+  String flightNumber;
+  HashSet<Seat> seats = new HashSet<Seat>();
 
-    public Flight(String flightNumber) {
-        this.flightNumber = flightNumber;
+  public Flight(String flightNumber) {
+    this.flightNumber = flightNumber;
+  }
+
+  public int getAvailableSeats() {
+    int availableSeats = 0;
+    for (Seat seat : seats) {
+      if (seat.isAvailable())
+        availableSeats++;
     }
+    return availableSeats;
+  }
 
-    public int getAvailableSeats() {
-        int availableSeats = 0;
-        for (Seat seat : seats) {
-            if (seat.isAvailable())
-            availableSeats++;
-        }
-        return availableSeats;
+  public String getFlightNumber() {
+    return flightNumber;
+  }
+
+  public void addSeat(Seat seat) {
+    seats.add(seat);
+  }
+
+  public int getCheapestSeat() {
+    int cheapest = Integer.MAX_VALUE;
+    for (Seat seat : seats) {
+      cheapest = cheapest > seat.getPrice() ? seat.getPrice() : cheapest;
     }
+    return cheapest;
+  }
 
-    public String getFlightNumber() {
-        return flightNumber;
+  public boolean containsSeat(String seatNumber) {
+    for (Seat seat : seats) {
+      if (seat.getNumber().equals(seatNumber))
+        return true;
     }
+    return false;
+  }
 
-    public void addSeat(Seat seat) {
-        seats.add(seat);
+  public boolean isSeatAvailable(String seatNumber) {
+      for (Seat seat : seats) {
+          if (seat.getNumber().equals(seatNumber))
+              return seat.isAvailable();
+      }
+    return false;
+  }
+
+  public String bookSeat() {
+    for (Seat seat : seats) {
+      if (seat.isAvailable()) {
+        seat.setAvailable(false);
+        return seat.getNumber();
+      }
+
     }
-
-    public int getCheapestSeat() {
-        int cheapest = Integer.MAX_VALUE;
-        for (Seat seat : seats) {
-            cheapest = cheapest > seat.getPrice()?seat.getPrice():cheapest;
-        }
-        return cheapest;
-    }
-
-    public boolean containsSeat(String seatNumber) {
-        return false;
-    }
-
-    public boolean isSeatAvailable(String seatNumber) {
-        return false;
-    }
-
-    public String bookSeat() {
-        for (Seat seat : seats) {
-            if (seat.isAvailable()) {
-                seat.setAvailable(false);
-                return seat.getNumber();
-            }
-
-        }
-        return null;
-    }
+    return null;
+  }
 }
