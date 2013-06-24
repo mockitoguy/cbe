f056c43a5fafd71a632cf0a719f1a26ae942dcf2
Patryk Grela <patryk.grela@payu.pl>
2013-06-24 13:42:44 +0200

    iteration 2

--- team5/src/main/java/Flight.java
+++ team5/src/main/java/Flight.java
@@ -10,7 +10,11 @@ public class Flight {
     private final String origin;
     private final String destination;
     private final Date date;
-    Map<Integer,Seat> seatsList = new HashMap<Integer,Seat>();
+
+    private int seats;
+    private String flightCode;
+    Map<Integer, Seat> seatsList = new HashMap<Integer, Seat>();
+
     public int getSeats() {
         return seats;
     }
@@ -31,16 +35,13 @@ public class Flight {
         return flightCode;
     }
 
-    private int seats;
-    private String flightCode;
-
     public Flight(String flightCode, int seats, int defaultPrice, String origin, String destination, Date date) {
         this.seats = seats;
         this.flightCode = flightCode;
         this.defaultPrice = defaultPrice;
-        this.origin=origin;
-        this.destination=destination;
-        this.date=date;
+        this.origin = origin;
+        this.destination = destination;
+        this.date = date;
 
         for (int i = 0; i < seats; i++) {
             seatsList.put(i + 1, new Seat(i + 1, defaultPrice));
@@ -48,17 +49,17 @@ public class Flight {
     }
 
     public void setSeatPrice(int seatNumber, int price) {
-        if(seatsList.get(seatNumber)==null)
+        if (seatsList.get(seatNumber) == null)
             throw new RuntimeException("No such seat!");
         seatsList.get(seatNumber).setPrice(price);
     }
 
     public int getCheapestSeatPrice() {
-        if(seatsList.isEmpty())
+        if (seatsList.isEmpty())
             throw new RuntimeException("No seats on the flight");
         Integer cheapest = null;
-        for(Seat seat: seatsList.values()){
-            if(cheapest==null || cheapest>seat.getPrice()){
+        for (Seat seat : seatsList.values()) {
+            if (cheapest == null || cheapest > seat.getPrice()) {
                 cheapest = seat.getPrice();
             }
         }
@@ -70,24 +71,97 @@ public class Flight {
     }
 
     public Seat getSeat(int seatNumber) {
-        if(seatsList.get(seatNumber)==null)
+        if (seatsList.get(seatNumber) == null)
             throw new RuntimeException("No such seat!");
         return seatsList.get(seatNumber);
     }
 
     public double getNotBookedSeatsAveragePrice() {
-        if(seatsList.isEmpty())
+        if (seatsList.isEmpty())
             throw new RuntimeException("No seats on the flight");
         double average = 0;
         int count = 0;
-        for(Seat seat: seatsList.values()){
-            if(seat.isBooked()==false ){
-                average=average+seat.getPrice();
+        for (Seat seat : seatsList.values()) {
+            if (seat.isBooked() == false) {
+                average = average + seat.getPrice();
                 count++;
             }
         }
-        if (count==0)
+        if (count == 0)
             throw new RuntimeException("No not booked seats");
-        return average/count;
+        return average / count;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+            "origin='" + origin + '\'' +
+            ", destination='" + destination + '\'' +
+            ", seats=" + seats +
+            '}';
+    }
+
+    public void setSeatsClass(int first, int last, Seat.CLASS seatClass) {
+        for (int i = first; i <= last; ++i) {
+            seatsList.get(i).setClass(seatClass);
+        }
+    }
+
+    public double getSeatsAveragePrice(Seat.CLASS seatClass) {
+        int conter = 0;
+        double average = 0;
+        for (Seat seat : seatsList.values()) {
+            if (seat.getaClass() == seatClass) {
+                average += seat.getPrice();
+                ++conter;
+            }
+        }
+        return average / conter;
+    }
+    public static FlightBuilder createBuilder(){
+          return new FlightBuilder();
+    }
+
+    public static class FlightBuilder {
+        public Flight build() {
+            return new Flight(flightCode, seats, defaultPrice, origin, destination, date);
+        }
+
+        public FlightBuilder withDefaultPrice(int defaultPrice) {
+            this.defaultPrice = defaultPrice;
+            return this;
+        }
+
+        public FlightBuilder withOrigin(String origin) {
+            this.origin = origin;
+            return this;
+        }
+
+        public FlightBuilder withDestination(String destination) {
+            this.destination = destination;
+            return this;
+        }
+
+        public FlightBuilder withDate(Date date) {
+            this.date = date;
+            return this;
+        }
+
+        public FlightBuilder withSeats(int seats) {
+            this.seats = seats;
+            return this;
+        }
+
+        public FlightBuilder withFlightCode(String flightCode) {
+            this.flightCode = flightCode;
+            return this;
+        }
+
+        private int defaultPrice=100;
+        private String origin;
+        private String destination;
+        private Date date;
+        private int seats=100;
+        private String flightCode;
     }
 }
--- team5/src/test/java/SeatsNumberTest.java
+++ team5/src/test/java/SeatsNumberTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.Date;
 import java.util.List;
 
+import org.fest.assertions.Delta;
 import org.junit.Test;
 
 /**
@@ -14,7 +15,7 @@ public class SeatsNumberTest {
     public void shouldTellNumberOfAvailableSeatsOnTheFlight() {
         //when
         FlightManager flightManager = new FlightManager();
-        Flight flight = new Flight("FR8723",5, 100, "Warsaw", "Chicago", new Date());
+        Flight flight = Flight.createBuilder().withFlightCode("FR8723").withSeats(5).build();
         flightManager.addFlight(flight);
 
         //given
@@ -25,55 +26,47 @@ public class SeatsNumberTest {
     }
     @Test public void shouldReturnCheapestSeatPrice(){
         //when
-        FlightManager flightManager = new FlightManager();
-        Flight flight = new Flight("FR8723",1,20, "Warsaw", "Chicago", new Date());
+        Flight flight = Flight.createBuilder().withDefaultPrice(100).withSeats(5).build();
         flight.setSeatPrice(2, 200);
-        flight.setSeatPrice(4,20);
-        flightManager.addFlight(flight);
+        flight.setSeatPrice(4, 20);
 
         //given
-        int cheapestSeatPrice = flightManager.getFlight("FR8723").getCheapestSeatPrice();
+        int cheapestSeatPrice = flight.getCheapestSeatPrice();
 
         //then
         assertEquals(20, cheapestSeatPrice);
     }
     @Test public void shouldBookASeat(){
         //when
-        FlightManager flightManager = new FlightManager();
-        Flight flight = new Flight("FR8723",10,20, "Warsaw", "Chicago", new Date());
-        flightManager.addFlight(flight);
+        Flight flight = Flight.createBuilder().withSeats(5).build();
 
         //given
-        flightManager.getFlight("FR8723").bookSeat(3);
+        flight.bookSeat(3);
 
         //then
-        Seat seat = flightManager.getFlight("FR8723").getSeat(3);
+        Seat seat = flight.getSeat(3);
         assertEquals(true, seat.isBooked());
     }
     @Test public void shouldNotBookASeat(){
         //when
-        FlightManager flightManager = new FlightManager();
-        Flight flight = new Flight("FR8723",10,20, "Warsaw", "Chicago", new Date());
-        flightManager.addFlight(flight);
+        Flight flight = Flight.createBuilder().withSeats(5).build();
 
         //given
-        flightManager.getFlight("FR8723").bookSeat(3);
+        flight.bookSeat(3);
 
         //then
-        Seat seat = flightManager.getFlight("FR8723").getSeat(4);
+        Seat seat = flight.getSeat(4);
         assertEquals(false, seat.isBooked());
     }
     @Test public void shouldReturnNotBookedSeatsAveragePrice(){
         //when
-        FlightManager flightManager = new FlightManager();
-        Flight flight = new Flight("FR8723",10,20, "Warsaw", "Chicago", new Date());
-        flightManager.addFlight(flight);
-        flightManager.getFlight("FR8723").bookSeat(3);
+        Flight flight = Flight.createBuilder().withSeats(10).withDefaultPrice(20).build();
         flight.setSeatPrice(3, 200);
         flight.setSeatPrice(1, 11);
+        flight.bookSeat(3);
 
         //given
-        double average = flightManager.getFlight("FR8723").getNotBookedSeatsAveragePrice();
+        double average = flight.getNotBookedSeatsAveragePrice();
 
         //then
         assertEquals(19, average,0.0001);
@@ -95,5 +88,56 @@ public class SeatsNumberTest {
 
         //then
         assertThat(flights).contains(flight1).contains(flight2);
+    }
+    @Test public void shouldReturnFlightsFromGivenOrigin(){
+        //when
+        FlightManager flightManager = new FlightManager();
+        Flight flight1 = new Flight("FR8723",10,20,"Warsaw","Chicago",new Date());
+        Flight flight2 = new Flight("FR2222",10,20,"Tokyo","Chicago",new Date());
+        Flight flight3 = new Flight("FR8724",10,20,"Warsaw","Tokyo",new Date());
+        Flight flight4 = new Flight("FR1111",10,20,"Warsaw","Poznan",new Date());
+        flightManager.addFlight(flight1);
+        flightManager.addFlight(flight2);
+        flightManager.addFlight(flight3);
+        flightManager.addFlight(flight4);
+
+
+        //given
+        List<Flight> flights = flightManager.getFlightsFrom("Warsaw");
+
+        //then
+        assertThat(flights).contains(flight1).contains(flight3).contains(flight4);
+    }
+    @Test public void shouldReturnFlightsFromGivenDestination(){
+        //when
+        FlightManager flightManager = new FlightManager();
+        Flight flight1 = new Flight("FR8723",10,20,"Warsaw","Chicago",new Date());
+        Flight flight2 = new Flight("FR2222",10,20,"Tokyo","Chicago",new Date());
+        Flight flight3 = new Flight("FR8724",10,20,"Warsaw","Tokyo",new Date());
+        Flight flight4 = new Flight("FR1111",10,20,"Warsaw","Poznan",new Date());
+        flightManager.addFlight(flight1);
+        flightManager.addFlight(flight2);
+        flightManager.addFlight(flight3);
+        flightManager.addFlight(flight4);
+
+
+        //given
+        List<Flight> flights = flightManager.getFlightsTo("Chicago");
+
+        //then
+        assertThat(flights).contains(flight1).contains(flight2);
+    }
+    @Test public void shouldReturnAveragePriceInAGivenClass(){
+        //when
+        Flight flight = new Flight("FR8723",100,20,"Warsaw","Chicago",new Date());
+        flight.setSeatsClass(5,12,Seat.CLASS.BUSSINESS);
+        flight.setSeatPrice(6,28);
+
+
+        //given
+        double average = flight.getSeatsAveragePrice(Seat.CLASS.BUSSINESS);
+
+        //then
+        assertThat(average).isEqualTo(21.0, Delta.delta(0.0001));
     }
 }
