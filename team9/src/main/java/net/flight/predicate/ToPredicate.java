b32b88d09f664c31f0b965d655c21126e148f807
Pawel Bobruk <pawel.bobruk@allegro.pl>
2013-06-25 16:43:28 +0200

    ITER-2 | refactor

--- team9/src/main/java/net/flight/predicate/ToPredicate.java
+++ team9/src/main/java/net/flight/predicate/ToPredicate.java
@@ -1,18 +1,19 @@
-package net.flight;
+package net.flight.predicate;
 
 import com.google.common.base.Predicate;
 import com.sun.istack.internal.Nullable;
+import net.flight.model.Flight;
 
-public class DestinationPredicate implements Predicate<Flight> {
+public class ToPredicate implements Predicate<Flight> {
 
   private final String destination;
 
-  public DestinationPredicate(String destination) {
+  public ToPredicate(String destination) {
     this.destination = destination;
   }
 
   @Override
-  public boolean apply(@Nullable net.flight.Flight flight) {
+  public boolean apply(@Nullable Flight flight) {
     return flight != null && destination.equals(flight.getDestination());
   }
 
