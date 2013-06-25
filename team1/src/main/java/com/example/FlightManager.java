ac3f23b0961c0df5f17bb27ea48ffdc8d2114358
rooster44 <rooster44@gmail.com>
2013-06-25 15:53:28 +0200

    example :)

--- team1/src/main/java/com/example/FlightManager.java
+++ team1/src/main/java/com/example/FlightManager.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class FlightManager {
@@ -17,7 +19,7 @@ public class FlightManager {
         if(flight==null) {
             throw new FlightManagerException("No such flight");
         }
-        return flight.getAvailableSeats();
+        return flight.getAvailableSeats().size();
     }
 
     public void addFlight(Flight flight) {
@@ -30,5 +32,35 @@ public class FlightManager {
             throw new FlightManagerException("No such flight");
         }
         return flight.getLowestSeatPrice();
+    }
+
+    public BookingStatus bookSeat(String flightNumber, Integer seatNumber) {
+        Flight flight = flights.get(flightNumber);
+        Seat seat = flight.getSeat(seatNumber);
+        if (seat.isAvailable()){
+            seat.setAvailable(false);
+            return BookingStatus.SUCCESS;
+        }
+        else {
+            return BookingStatus.FAIL;
+        }
+
+    }
+
+    public BigDecimal getAveragePriceOfAvailableSeats(String flightNumber) {
+        Flight flight = flights.get(flightNumber);
+        List<Seat> availableSeats = flight.getAvailableSeats();
+        BigDecimal totalPriceOfAvailableSeats = new BigDecimal("0.00");
+        for (Seat availableSeat : availableSeats) {
+            totalPriceOfAvailableSeats = totalPriceOfAvailableSeats.add(availableSeat.getPrice());
+        }
+        BigDecimal averagePrice = totalPriceOfAvailableSeats.divide(new BigDecimal(availableSeats.size()));
+        return averagePrice;
+    }
+
+    public List<Flight> findFlights(String from, String to) {
+        List <Flight> flights = new ArrayList<Flight>();
+
+        return flights;
     }
 }
