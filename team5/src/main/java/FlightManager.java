a938ad11b91164830541aab9df072dce1ba6aed4
Slawomir Bolimowski <slawomir.bolimowski@allegro.pl>
2014-01-31 15:38:47 +0100

    iteration 2

--- team5/src/main/java/FlightManager.java
+++ team5/src/main/java/FlightManager.java
@@ -1,5 +1,10 @@
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,6 +31,10 @@ public class FlightManager {
         findFlight(flightNumber).bookSeat(seatNumber);
     }
 
+    public BigDecimal getAveragePriceOfNonBookedSeats(String flightNumber) {
+        return findFlight(flightNumber).getAveragePrice();
+    }
+
     private Flight findFlight(String flightNumber) {
         if (flights.containsKey(flightNumber)) {
             return flights.get(flightNumber);
@@ -33,4 +42,35 @@ public class FlightManager {
             throw new NoFlightFoundException(flightNumber);
         }
     }
+
+    public List<Flight> getNumberOfFlights(final String origin, final String destination) {
+        return Lists.newArrayList(Collections2.filter(flights.values(), new Predicate<Flight>() {
+            @Override
+            public boolean apply(Flight flight) {
+                return flight.matchOriginAndDestination(origin, destination);
+            }
+        }));
+    }
+
+    public List<Flight> getNumberOfFlightsForOrigin(final String origin) {
+        return Lists.newArrayList(Collections2.filter(flights.values(), new Predicate<Flight>() {
+            @Override
+            public boolean apply(Flight flight) {
+                return flight.matchOrigin(origin);
+            }
+        }));
+    }
+
+    public List<Flight> getNumberOfFlightsToDestination(final String destination) {
+        return Lists.newArrayList(Collections2.filter(flights.values(), new Predicate<Flight>() {
+            @Override
+            public boolean apply(Flight flight) {
+                return flight.matchDestination(destination);
+            }
+        }));
+    }
+
+
+
+
 }
